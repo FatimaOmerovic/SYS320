0640702b571af270b6e0d5221ff11eb0cce88f37
Fatima <97912341+FatimaOmerovic@users.noreply.github.com>
2023-10-30 19:46:31 -0400

    Add files via upload

---
+++ Week 9/PowershellScriptforSYS.ps1
@@ -0,0 +1,16 @@
+﻿# Task: Grab the network adapter information using the WMI class
+# Get the IP address, default gateway, and the DNS servers.
+# BONUS: Get the DHCP server.
+# Running your code using a screen recorder.
+
+# Task 1: 
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -eq $true } | Select-Object -Property DHCPServer, DNSServerSearchOrder
+
+# Task 2:
+Get-Process | Export-Csv -Path "C:\Users\champuser\Desktop\ProcessesRunning.csv" -NoTypeInformation
+Get-Service | Where-Object { $_.Status -eq "Running" } | Export-Csv -Path "C:\Users\champuser\Desktop\ServicesRunning.csv" -NoTypeInformation
+
+# Task 3:
+Start-Process -Name "calc"
+Get-Process -Name "calc*" | Stop-Process -Force
+Write-Output "You just killed the calculator :("
